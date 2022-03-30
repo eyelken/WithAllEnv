0edddd1c03812dca9113a306fb0bb6dd961784e3
OscarSah <oscar@cybertekschool.com>
2022-03-30 18:50:16 +0300

    POM pages

--- src/test/java/com/cydeo/pages/BasePage.java
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -1,9 +1,10 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public abstract class BasePage {
     public BasePage(){
-        PageFactory.initElements();
+        PageFactory.initElements(Driver.get(),this);
     }
 }

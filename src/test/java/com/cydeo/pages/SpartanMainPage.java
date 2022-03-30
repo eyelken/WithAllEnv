ee787e18be8eabe27c27fd0b6e6e216829204043
OscarSah <oscar@cybertekschool.com>
2022-03-30 20:56:13 +0300

    Cucumber Framework started

---
+++ src/test/java/com/cydeo/pages/SpartanMainPage.java
@@ -0,0 +1,15 @@
+package com.cydeo.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SpartanMainPage extends SpartanBasePage{
+
+// create a useful method when I pass ID number, it should return me view button of that spartan
+
+    public WebElement getSpartanView(int id){
+        //  //tbody//tr//td[.='41']/../td[5]
+    }
+
+
+}
--- src/test/java/com/cydeo/step_definitions/SpartansUISteps.java
+++ src/test/java/com/cydeo/step_definitions/SpartansUISteps.java
@@ -1,5 +1,7 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,7 +9,7 @@ import io.cucumber.java.en.When;
 public class SpartansUISteps {
     @Given("Commander is at the main page")
     public void commander_is_at_the_main_page() {
-
+        Driver.get().get(ConfigurationReader.get("spartan.webUrl"));
     }
     @When("Commander clicks on view button of the spartan with the ID number {int}")
     public void commander_clicks_on_view_button_of_the_spartan_with_the_id_number(Integer int1) {

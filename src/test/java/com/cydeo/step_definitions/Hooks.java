3d3d3acf9942469f5dabb8ab3b2dab1bfeb69614
OscarSah <oscar@cybertekschool.com>
2022-03-31 21:46:33 +0300

    Hooks renewed

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -7,27 +7,23 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 
+import java.util.concurrent.TimeUnit;
+
 import static io.restassured.RestAssured.*;
 
 public class Hooks {
 
-//    @Before(value = "@mock", order=0)
-//    public void setUpMock(){
-//        baseURI= ConfigurationReader.get("mock.apiUrl");
-//    }
-//    @Before(value = "@test",order=1)
-//    public void setUpTest(){
-//        baseURI= ConfigurationReader.get("spartan.apiUrl");
-//    }
+
 
     @Before
     public void setUp(){
-        System.out.println("Some methods before Test Cases");
+        Driver.get().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
     }
 
     @After
     public void tearDown(){
-      //  Driver.closeDriver();
+       Driver.closeDriver();
     }
 
     @Before("@db")
